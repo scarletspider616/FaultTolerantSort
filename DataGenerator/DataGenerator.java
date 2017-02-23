4b9d766869b1b193ec55c4f2d305bc73dbfbdcac
scarletspider616 <j.fallone@ualberta.ca>
2017-02-23 01:22:37 -0700

    Added barebones (not yet implemented) datagenerator code

---
+++ DataGenerator/DataGenerator.java
@@ -0,0 +1,20 @@
+package project1.datagen;
+
+// import java.io.BufferedWriter;
+// import java.io.FileWriter;
+// import java.io.IOException;
+
+public class DataGenerator {
+
+	/**
+	* Constructor for DataGenerator Class. Takes in number of random values 
+	* to generate, as well as the output filename (context: pwd). 
+	* The constructor will automatically execute the random number generation 
+	* and then write the data to the file.
+	**/
+	public DataGenerator(String outputFileName, int numOfValues) {
+		// TODO: Write to file implementation 
+		// can i use random.org truly random values with open source API
+		// or do i need to actually make a pseudorandom generator w/ seed? 
+	}
+}

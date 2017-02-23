ca759c0c03a54a339880b29a133ad15adc54f37c
scarletspider616 <j.fallone@ualberta.ca>
2017-02-23 01:23:03 -0700

    Added Barebones sorting code, started implementation of insertion sort (backup)

---
+++ Sort/insertion_sort.c
@@ -0,0 +1,10 @@
+// insertion sort implementation in C based on pseudo code found at: 
+// https://en.wikipedia.org/wiki/Insertion_sort
+
+// operating under assumption that a checkpoint has been saved elsewhere
+// so insertion sort will in fact rewrite the input data in the memory 
+// provided to it.
+
+// How this will relate into java calling it as a thread and then a native
+// C method could possibly change things moving forward. At this point unclear
+void insertion_sort()

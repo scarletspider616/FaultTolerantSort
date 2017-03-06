c0c276545b5e4fe8a875cc9dc6d2926fa4b94695
scarletspider616 <j.fallone@ualberta.ca>
2017-03-06 11:23:29 -0700

    Fixed bgu

--- DataGenerator/DataGenerator.java
+++ DataGenerator/DataGenerator.java
@@ -1,5 +1,10 @@
 package project1.datagen;
 
+import java.util.concurrent.ThreadLocalRandom;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 // import java.io.BufferedWriter;
 // import java.io.FileWriter;
 // import java.io.IOException;
@@ -13,9 +18,51 @@ public class DataGenerator {
 	* and then write the data to the file.
 	**/
 	public DataGenerator(String outputFileName, int numOfValues) {
-		int randomValues[numOfValues];
+		int randomValues[] = new int [numOfValues];
 		for(int i=0; i < numOfValues; i++) {
 				randomValues[i] = getRandomInt();
 		}
+		writeValues(randomValues, outputFileName);
+	}
+
+	public static void main(String [] args) {
+		try {
+			DataGenerator gen = new DataGenerator(args[0],
+			 Integer.parseInt(args[1]));
+		} catch (Exception e) {
+			System.out.println("Please enter args: 'filename' 'no. of ints'");
+			System.exit(-1);
+		}
+	}
+
+	private int getRandomInt() {
+		// http://stackoverflow.com/questions/363681/generating-random-integers-in-a-specific-range
+		return ThreadLocalRandom.current().nextInt(Integer.MIN_VALUE, 
+			Integer.MAX_VALUE);
+	}
+
+	private void writeValues(int[] writeValues, String outputFilename) {
+		System.out.println("Random Values Produced: ");
+		for(int r: writeValues) {
+			System.out.print(r);
+			System.out.print(" ");
+		}
+		System.out.println(" ");
+		// based on:
+		// http://stackoverflow.com/questions/12350248/java-difference-between-filewriter-and-bufferedwriter
+		try {
+		    BufferedWriter bw = new BufferedWriter(
+		    	new FileWriter(outputFilename));
+		    String output = "";
+		    for(int number: writeValues) {
+		    	output = output + Integer.toString(number) + " ";
+		    }
+			bw.write(output);
+			bw.flush();
+		 } catch (IOException e) {
+			e.printStackTrace();
+		}
+	    System.out.println(
+	    	"Random vals should be available in " + outputFilename);
 	}
 }
